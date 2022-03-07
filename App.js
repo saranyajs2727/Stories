54271d032dc8dcf5c4dc091872cc38632574589b
saranya <saranyac2727@gmail.com>
2022-03-07 18:50:17 +0530

    Stories

---
+++ App.js
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Home from './src/Components/Home'
+
+
+
+const App = () => {
+  return (
+    <View>
+    <Home />
+    
+    </View>
+  )
+}
+
+export default App
