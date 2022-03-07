29ed8ad4ae1d030d5408d247703a159adae5fa0c
saranya <saranyac2727@gmail.com>
2022-03-07 20:34:34 +0530

    stories Added

--- App.js
+++ App.js
@@ -2,21 +2,23 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Status from './src/Components/Status';
 
+import BottomTabView from './src/Components/BottomTabView';
+import Status from './src/Components/Status';
+import Home from "./src/Components/Home"
 const App = () => {
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-    <Stack.Navigator
-      screenOptions={{
-        headerShown: false,
-      }}>
-  
+    <Stack.Navigator screenOptions={{
+          headerShown: false,
+        }}>
+    {/* <Stack.Screen name="BottomTabView" component={BottomTabView} /> */}
+      <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="Status" component={Status} />
-
     </Stack.Navigator>
   </NavigationContainer>
+
   )
 }
 
